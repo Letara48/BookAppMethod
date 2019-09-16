c7b459d2fe3a41bbe5d473c36a10c75c9dc88883
letara48 <lgirl96@hotmail.com>
2019-09-16 14:19:57 -0400

    Book application

---
+++ src/BookApp.java
@@ -0,0 +1,7 @@
+public class BookApp {
+    public static void main(String[] args) {
+        Book charlottes_web = new Book("Charlotte's Web", "E.B. White", "A children's story about a friendship between a spider and pig",14.99, false);
+        charlottes_web.getDisplayText();
+    }
+
+}
